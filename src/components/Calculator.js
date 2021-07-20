5c2ba84550048998202ad456d0ecb088d000fb7e
choi woo seok <cwooseok60@gmail.com>
2021-07-20 22:20:08 +0900

    first commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,31 @@
+
+function Calculator() {
+
+  const add = () => {
+
+  }
+
+  const minus = () => {
+
+  }
+
+  const multiple = () => {
+
+  }
+
+  const divide = () => {
+
+  }
+  return (
+    <>
+      <label>a : </label><input type="text"/>
+      <label>operator : </label><input type='text'/>
+      <label>b : </label><input type="text"/>
+
+      <p>result : {}</p>
+    </>
+  )
+}
+
+
+export default Calculator;
